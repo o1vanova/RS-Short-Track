71724f1edbcf16b89ff8f63fa3e225d1608a49f6
Olga Ivanova <olgai1409@gmail.com>
2020-01-17 16:53:40 +0500

    feat: add empty files

---
+++ custom-lodash/src/array-tasks.js
@@ -0,0 +1,15 @@
+/**
+ * Creates an array of elements split into groups the length of size.
+ * If array can't be split evenly, the final chunk will be the remaining elements.
+ *
+ * @param {array} array The array to process
+ * @param {number} size The length of each chunk. 1 by default
+ * @return {array} Returns the new array of chunks.
+ *
+ * @example
+ *    _.chunk(['a', 'b', 'c', 'd'], 2) => [['a', 'b'], ['c', 'd']]
+ *    _.chunk(['a', 'b', 'c', 'd'], 3) => [['a', 'b', 'c'], ['d']]
+ */
+function chunk(array, [size = 1]) {
+  return array;
+}
